25b9ce8e051502f61988136a78975a351701f089
JakobPriesner <68620674+JakobPriesner@users.noreply.github.com>
2020-09-02 02:14:43 +0200

    Modul, damit man leichter schauen kann, welche module nicht funktionieren
    Ps: Klappt nur für Server-Module

---
+++ server/modules/fehlerhafte_module.py
@@ -0,0 +1,18 @@
+
+def isValid(text):
+	text = text.lower()
+	if 'module' in text and 'fehlerhaft' in text:
+		return True
+	elif 'module' in text and 'funktionieren' in text and 'nicht' in text:
+		return True
+
+def handle(text, luna, profile):
+	faulty_list = []
+	for module in luna.local_storage['modules'].values():
+		if module['status'] == 'error':
+			faulty_list.append(module['name'])
+	print(faulty_list)
+	luna.say('Folgende Module konnten nicht geladen werden: ')
+	luna.say(luna.enumerate(faulty_list))
+	
+
