c989727c3ce6d7311e2d1d58f61ee7c75fdca100
JakobPriesner <68620674+JakobPriesner@users.noreply.github.com>
2020-09-02 02:17:07 +0200

    auflisten aller fehlerhaften Module
    PS: Funktioniert leider bis jetzt nur für die Server-Modules

--- server/modules/fehlerhafte_module.py
+++ server/modules/fehlerhafte_module.py
@@ -6,13 +6,29 @@ def isValid(text):
 	elif 'module' in text and 'funktionieren' in text and 'nicht' in text:
 		return True
 
-def handle(text, luna, profile):
+def handle(text, tiane, profile):
 	faulty_list = []
-	for module in luna.local_storage['modules'].values():
+	for module in tiane.local_storage['modules'].values():
 		if module['status'] == 'error':
 			faulty_list.append(module['name'])
 	print(faulty_list)
-	luna.say('Folgende Module konnten nicht geladen werden: ')
-	luna.say(luna.enumerate(faulty_list))
+	tiane.say('Folgende Module konnten nicht geladen werden: ')
+	tiane.say(tiane.enumerate(faulty_list))
 	
 
+def enumerate(self, array):
+            new_array = [] # array=['Apfel', 'Birne', 'Gemüse', 'wiederlich']
+            for item in array:
+                new_array.append(item.strip(' '))
+                
+            ausgabe = ''
+            if len(new_array) == 0:
+                pass
+            elif len(new_array) == 1:
+                ausgabe = array[0]
+            else:
+                for item in range(len(new_array) - 1):
+                    ausgabe += new_array[item] + ', '
+                ausgabe = ausgabe.rsplit(', ', 1)[0]
+                ausgabe = ausgabe + ' und ' + new_array[-1]
+            return ausgabe
